ebd091b3302d0b071f29639c729abf1a9c875b84
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-04 14:34:35 +0200

    complete DatabaseInitService

--- src/main/java/org/example/DatabaseInitService.java
+++ src/main/java/org/example/DatabaseInitService.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.flywaydb.core.Flyway;
+
+import static org.example.Database.*;
+
 public class DatabaseInitService {
     public void initDatabase() {
-
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(DB_URL, DB_USER, null)
+                .load();
+        flyway.migrate();
     }
 }

eb471aad7d82ff372f4545f0cde73a66eed654c4
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-04 14:46:55 +0200

    complete Client entity

--- src/main/java/org/example/client/Client.java
+++ src/main/java/org/example/client/Client.java
@@ -2,12 +2,18 @@ package org.example.client;
 
 public class Client {
     private String name;
+    private int id;
 
-    public Client(String name) {
+    public Client(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
     }
 }

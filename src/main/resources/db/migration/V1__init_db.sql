9fa56e2491f6196e84d16170e2d054a2c0a9218a
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-05 11:06:21 +0200

    fix sql files

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,28 +1,34 @@
-CREATE TABLE IF NOT EXISTS worker (
-    id INT PRIMARY KEY,
-    name VARCHAR(1000) NOT NULL CHECK (LENGTH(name) >= 2 AND LENGTH(name) <= 1000),
-    birthday DATE CHECK (birthday >= '1900-01-01'),
-    level ENUM('Trainee', 'Junior', 'Middle', 'Senior') NOT NULL,
-    salary INT CHECK (salary >= 100 AND salary <= 100000)
+CREATE TABLE worker (
+id IDENTITY PRIMARY KEY,
+name VARCHAR(1000) NOT NULL CHECK (LENGTH(name) >= 2),
+birthday DATE CHECK (YEAR(birthday) > 1900),
+level ENUM('Trainee', 'Junior', 'Middle', 'Senior') NOT NULL,
+salary INTEGER CHECK (salary >= 100 AND salary <= 100000)
 );
 
-CREATE TABLE IF NOT EXISTS client (
-    id INT PRIMARY KEY,
-    name VARCHAR(1000) NOT NULL CHECK (LENGTH(name) >= 2 AND LENGTH(name) <= 1000)
+
+
+CREATE TABLE client(
+id IDENTITY PRIMARY KEY,
+name VARCHAR(1000) NOT NULL CHECK (LENGTH(name) >= 2)
 );
 
-CREATE TABLE IF NOT EXISTS project (
-    id INT PRIMARY KEY,
-    client_id INT,
-    start_date DATE,
-    finish_date DATE,
-    FOREIGN KEY (client_id) REFERENCES client(id)
+
+
+CREATE TABLE project(
+id IDENTITY PRIMARY KEY,
+client_id BIGINT NOT NULL,
+start_date DATE,
+finish_date DATE,
+FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS project_worker (
-     project_id INT,
-     worker_id INT,
-     PRIMARY KEY (project_id, worker_id),
-     FOREIGN KEY (project_id) REFERENCES project(id),
-     FOREIGN KEY (worker_id) REFERENCES worker(id)
+
+
+CREATE TABLE project_worker(
+project_id BIGINT NOT NULL,
+worker_id BIGINT NOT NULL,
+PRIMARY KEY (project_id, worker_id),
+FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
+FOREIGN KEY (worker_id) REFERENCES worker(id) ON DELETE CASCADE
 );
